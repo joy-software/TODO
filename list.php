eed0fe920fd7e210b5394eac8d6a5fdd8608b0fc
Joy  Ndjama <joyjedid@gmail.com>
2018-02-18 13:30:10 +0100

    Docker local

--- list.php
+++ list.php
@@ -9,10 +9,10 @@
 
 <?php
 /*Database Connection*/
- $host = 'eu-cdbr-west-02.cleardb.net';
-  $username = 'b1be3a26d2d39d';
-  $password = '22bb9a5f';
-  $database = 'heroku_ab050365848a9e1';
+ $host = 'mysql';
+  $username = 'root';
+  $password = 'todoappbd';
+  $database = 'dbapp';
   Global $dbconfig;// to use globally
 $dbconfig = mysqli_connect($host,$username,$password,$database) or die("An Error occured while connecting to the database");
 ?>
